0509bb623abbfded32c4f50faf6a718ec0f882ca
Yan Santos Policarpo <h4rd1i@gmail.com>
2020-03-28 01:33:24 -0300

    C:/Program Files/Git/docs

---
+++ docs/precache-manifest.8ad0673822043ee623b897985cc068e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98faa8d9852220e026d3404a76e27448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ced36f0d17090b480c9",
+    "url": "/static/css/main.2bf237ef.chunk.css"
+  },
+  {
+    "revision": "2d2349af27ea858bc2a9",
+    "url": "/static/js/2.27fe0b90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27fe0b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ced36f0d17090b480c9",
+    "url": "/static/js/main.6b0550a7.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
